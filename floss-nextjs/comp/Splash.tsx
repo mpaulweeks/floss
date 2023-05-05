dc9a9098a3edcf016ea041721678fc3c9530c56a
mpaulweeks <mpaulweeks@gmail.com>
2023-05-05 15:06:34 -0700

    style splash

--- floss-nextjs/comp/Splash.tsx
+++ floss-nextjs/comp/Splash.tsx
@@ -12,27 +12,31 @@ export function Splash(props: {
         <div>
           <WelcomePreview settings={props.settings} />
         </div>
-        <p>
+        <div>
           hover your mouse in the bottom right corner for options
-        </p>
-        <p>
+        </div>
+        <div>
           play music?
           <label id="toggleMusic" className={styles.switch}>
             <input id="musicCheckbox" type="checkbox" />
             <span className={[styles.slider, styles.round].join(' ')}></span>
           </label>
-        </p>
-        <p>
+        </div>
+        <div>
           <button onClick={props.onConfirm}>
             LET THE PARTY BEGIN
           </button>
-        </p>
-        <p>
-          a silly thing made by
-          <a href="https://twitter.com/mpaulweeks">@mpaulweeks</a>
-          <br/>with artwork by
-          <a href="http://instagram.com/amyjxu">@amyjxu</a>
-        </p>
+        </div>
+        <div>
+          <div>
+            {' a silly thing made by '}
+            <a href="https://twitter.com/mpaulweeks">@mpaulweeks</a>
+          </div>
+          <div>
+            {' with artwork by '}
+            <a href="http://instagram.com/amyjxu">@amyjxu</a>
+          </div>
+        </div>
       </main>
     </div>
   );
