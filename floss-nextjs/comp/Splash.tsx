c75d0b026a50f1ae940b406ed37a04aaa486f1fb
mpaulweeks <mpaulweeks@gmail.com>
2023-05-06 23:54:34 -0400

    clean up splash

--- floss-nextjs/comp/Splash.tsx
+++ floss-nextjs/comp/Splash.tsx
@@ -1,15 +1,12 @@
 import styles from '@/styles/Splash.module.css';
 import { WelcomePreview } from './WelcomePreview';
 import { Settings } from '@/types';
-import { useState } from 'react';
 import rainbowStyles from '@/styles/Rainbow.module.css';
 
 export function Splash(props: {
   settings: Settings;
   onConfirm(): void;
 }) {
-  const [play, setPlay] = useState(false);
-
   return (
     <div className={[styles.welcome, rainbowStyles.rainbow].join(' ')}>
       <main className={styles.instructions}>
@@ -17,12 +14,8 @@ export function Splash(props: {
           <WelcomePreview settings={props.settings} />
         </div>
         <div>
-          hover your mouse in the bottom right corner for options
+          move your mouse to the right for options
         </div>
-        {/* <div>
-          play music?
-          <Slider checked={play} onClick={checked => setPlay(checked)} />
-        </div> */}
         <div>
           <button className={rainbowStyles.hoverbow} onClick={props.onConfirm}>
             LET THE PARTY BEGIN
