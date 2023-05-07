e77de717f71a7e2552b194e00e293a4831909248
mpaulweeks <mpaulweeks@gmail.com>
2023-05-06 22:46:45 -0400

    fix overlay hover

--- floss-nextjs/comp/Overlay.tsx
+++ floss-nextjs/comp/Overlay.tsx
@@ -1,7 +1,6 @@
 import { AnimationDance, AnimationLayout, AudioSrc, Settings, defaultSettings } from "@/types";
 import { Dispatch, SetStateAction } from "react";
 import styles from '@/styles/Overlay.module.css';
-import hoverStyles from '@/styles/Hover.module.css';
 import { useAudio } from "@/hooks/useAudio";
 
 export function Overlay(props: {
@@ -15,116 +14,118 @@ export function Overlay(props: {
     : play(audioSrc);
 
   return (
-    <section className={[styles.overlay, hoverStyles.hover].join(' ')}>
-      <div>
-        {'audio: '}
-        <button onClick={onAudioClick}>
-          {isPlaying ? 'ON' : 'OFF'}
-      </button>
-      </div>
-      <div>
-        <button onClick={() => props.setSettings(s => ({
-          ...s,
-          dance: AnimationDance.Floss,
-        }))}>
-          floss
+    <aside className={styles.overlayOuter}>
+      <section className={styles.overlayInner}>
+        <div>
+          {'audio: '}
+          <button onClick={onAudioClick}>
+            {isPlaying ? 'ON' : 'OFF'}
         </button>
-        <button onClick={() => props.setSettings(s => ({
-          ...s,
-          dance: AnimationDance.Shuffle,
-        }))}>
-          shuffle
-        </button>
-      </div>
-      <div>
-        <button onClick={() => props.setSettings(s => ({
-          ...s,
-          layout: AnimationLayout.Still
-        }))}>
-          still
-        </button>
-        <button onClick={() => props.setSettings(s => ({
-          ...s,
-          layout: AnimationLayout.Row
-        }))}>
-          row
-        </button>
-        <button onClick={() => props.setSettings(s => ({
-          ...s,
-          layout: AnimationLayout.Column
-        }))}>
-          col
-        </button>
-        <button onClick={() => props.setSettings(s => ({
-          ...s,
-          layout: AnimationLayout.Twirl
-        }))}>
-          twirl
-        </button>
-      </div>
-      <div>
-        {props.settings.count}
-        <button onClick={() => props.setSettings(s => ({
-          ...s,
-          count: Math.max(1, s.count - 1),
-        }))}>
-          fewer
-        </button>
-        <button onClick={() => props.setSettings(s => ({
-          ...s,
-          count: defaultSettings().count,
-        }))}>
-          reset
-        </button>
-        <button onClick={() => props.setSettings(s => ({
-          ...s,
-          count: Math.min(10, s.count + 1),
-        }))}>
-          more
-        </button>
-      </div>
-      <div>
-        {props.settings.danceSpeed}
-        <button onClick={() => props.setSettings(s => ({
-          ...s,
-          danceSpeed: Math.max(0, s.danceSpeed - 1),
-        }))}>
-          dance slow
-        </button>
-        <button onClick={() => props.setSettings(s => ({
-          ...s,
-          danceSpeed: defaultSettings().danceSpeed,
-        }))}>
-          reset
-        </button>
-        <button onClick={() => props.setSettings(s => ({
-          ...s,
-          danceSpeed: Math.min(10, s.danceSpeed + 1),
-        }))}>
-          dance fast
-        </button>
-      </div>
-      <div>
-        {props.settings.moveSpeed}
-        <button onClick={() => props.setSettings(s => ({
-          ...s,
-          moveSpeed: Math.max(1, s.moveSpeed - 1),
-        }))}>
-          move slow
-        </button>
-        <button onClick={() => props.setSettings(s => ({
-          ...s,
-          moveSpeed: defaultSettings().moveSpeed,
-        }))}>
-          reset
-        </button>
-        <button onClick={() => props.setSettings(s => ({
-          ...s,
-          moveSpeed: Math.min(10, s.moveSpeed + 1),
-        }))}>
-          move fast
-        </button>
-      </div>
-    </section>
-  )
+        </div>
+        <div>
+          <button onClick={() => props.setSettings(s => ({
+            ...s,
+            dance: AnimationDance.Floss,
+          }))}>
+            floss
+          </button>
+          <button onClick={() => props.setSettings(s => ({
+            ...s,
+            dance: AnimationDance.Shuffle,
+          }))}>
+            shuffle
+          </button>
+        </div>
+        <div>
+          <button onClick={() => props.setSettings(s => ({
+            ...s,
+            layout: AnimationLayout.Still
+          }))}>
+            still
+          </button>
+          <button onClick={() => props.setSettings(s => ({
+            ...s,
+            layout: AnimationLayout.Row
+          }))}>
+            row
+          </button>
+          <button onClick={() => props.setSettings(s => ({
+            ...s,
+            layout: AnimationLayout.Column
+          }))}>
+            col
+          </button>
+          <button onClick={() => props.setSettings(s => ({
+            ...s,
+            layout: AnimationLayout.Twirl
+          }))}>
+            twirl
+          </button>
+        </div>
+        <div>
+          {props.settings.count}
+          <button onClick={() => props.setSettings(s => ({
+            ...s,
+            count: Math.max(1, s.count - 1),
+          }))}>
+            fewer
+          </button>
+          <button onClick={() => props.setSettings(s => ({
+            ...s,
+            count: defaultSettings().count,
+          }))}>
+            reset
+          </button>
+          <button onClick={() => props.setSettings(s => ({
+            ...s,
+            count: Math.min(10, s.count + 1),
+          }))}>
+            more
+          </button>
+        </div>
+        <div>
+          {props.settings.danceSpeed}
+          <button onClick={() => props.setSettings(s => ({
+            ...s,
+            danceSpeed: Math.max(0, s.danceSpeed - 1),
+          }))}>
+            dance slow
+          </button>
+          <button onClick={() => props.setSettings(s => ({
+            ...s,
+            danceSpeed: defaultSettings().danceSpeed,
+          }))}>
+            reset
+          </button>
+          <button onClick={() => props.setSettings(s => ({
+            ...s,
+            danceSpeed: Math.min(10, s.danceSpeed + 1),
+          }))}>
+            dance fast
+          </button>
+        </div>
+        <div>
+          {props.settings.moveSpeed}
+          <button onClick={() => props.setSettings(s => ({
+            ...s,
+            moveSpeed: Math.max(1, s.moveSpeed - 1),
+          }))}>
+            move slow
+          </button>
+          <button onClick={() => props.setSettings(s => ({
+            ...s,
+            moveSpeed: defaultSettings().moveSpeed,
+          }))}>
+            reset
+          </button>
+          <button onClick={() => props.setSettings(s => ({
+            ...s,
+            moveSpeed: Math.min(10, s.moveSpeed + 1),
+          }))}>
+            move fast
+          </button>
+        </div>
+      </section>
+    </aside>
+  );
 }
